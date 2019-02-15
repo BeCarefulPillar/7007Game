889acb2dc2fa29022e66739fbd5db5ac6f2488f0
sunkang <kang.sun@joywinds.com>
2019-02-15 18:05:22 +0800

    add uiManager

---
+++ Assets/Code/XLua/LuaHelperManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using XLua;
+
+[LuaCallCSharp]
+public class LuaHelperManager {
+    protected static LuaHelperManager mInstance;
+    protected LuaHelperManager() {}
+    private static GameObject parent = GameObject.Find("Canvas");
+    public static LuaHelperManager Instance {
+        get {
+            if(mInstance == null) {
+                mInstance = new LuaHelperManager();
+            }
+            return mInstance;
+        }
+    }
+
+    /// <summary>
+    /// Xlua层加载UI面板
+    /// </summary>
+    /// <param name="path"> 路径 </param>
+    /// <param name="OnCreate"> 创建出来的委托回调 </param>
+    public void LoadUI(string path, XLuaCustomExport.OnCreate onCreate) {
+        Debug.Log("加载UI窗口 =========== " + path);
+        GameObject obj =  GameObject.Instantiate(Resources.Load<GameObject>(path), parent.transform);
+        if (onCreate != null)
+        {
+            obj.AddComponent<LuaViewBehaviour>();
+            onCreate(obj);
+        }
+    }
+}
+
+/// <summary>
+/// XLua的自定义拓展
+/// </summary>
+/// 
+public static class XLuaCustomExport {
+    [CSharpCallLua]
+    public delegate void OnCreate(GameObject obj);
+}
+
---
+++ Assets/Code/XLua/LuaMgr.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using UnityEngine;
+using XLua;
+
+public class LuaMgr : MonoBehaviour {
+    public static LuaMgr Instance;
+    public static LuaEnv luaEnv;
+    public static readonly string editorLuaPath = "Assets/Code/Lua";
+
+    private void Awake() {
+        DontDestroyOnLoad(this);
+        Instance = this;
+        luaEnv = new LuaEnv();
+
+        //设置xlua脚本路径 路径根据实际需求修改
+        luaEnv.AddLoader(LuaMgr.MyLoader);
+        //luaEnv.DoString(string.Format("package.path = '{0}/Code/Lua/?.lua'", Application.dataPath));
+    }
+
+    public void DoString(string str) {
+        luaEnv.DoString(str);
+    }
+
+    private static byte[] MyLoader(ref string filePath) {
+        string path = filePath.Replace(".", "/");
+
+        var code = UnityEditor.AssetDatabase.LoadAssetAtPath(editorLuaPath + "/" + path + ".lua.txt", typeof(TextAsset)) as TextAsset;
+        if (code != null) {
+            return code.bytes;
+        }
+        return null;
+    }
+}
