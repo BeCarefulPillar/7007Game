f2f64bd687f30e7f4aee26372b954a186a11e28e
sunkang <kang.sun@joywinds.com>
2020-03-30 17:35:55 +0800

    up

--- Assets/Code/XLua/LuaCallCSharpConfig.cs
+++ Assets/Code/XLua/LuaCallCSharpConfig.cs
@@ -1,9 +1,9 @@
-﻿// class LuaCallCSharpConfig {
-//     [XLua.LuaCallCSharp]
-//     public static System.Collections.Generic.List<System.Type> TYPES = new System.Collections.Generic.List<System.Type> {
-//         typeof(UnityEngine.Component),
-//         typeof(UnityEngine.GameObject),
-//         typeof(UnityEngine.Transform),
-//         typeof(LuaHelperManager),
-//     };
-// }
+﻿class LuaCallCSharpConfig {
+    [XLua.LuaCallCSharp]
+    public static System.Collections.Generic.List<System.Type> TYPES = new System.Collections.Generic.List<System.Type> {
+        typeof(UnityEngine.Component),
+        typeof(UnityEngine.GameObject),
+        typeof(UnityEngine.Transform),
+        typeof(LuaHelperManager),
+    };
+}
