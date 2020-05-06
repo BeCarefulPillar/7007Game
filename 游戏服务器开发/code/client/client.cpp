60d0fd13abf3544511743d621e5a8e788052feb1
sunkang <kang.sun@joywinds.com>
2020-05-06 17:05:17 +0800

    up

--- 游戏服务器开发/code/client/client.cpp
+++ 游戏服务器开发/code/client/client.cpp
@@ -1,20 +1,45 @@
-﻿// client.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
+﻿#include <iostream>
 
-#include <iostream>
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN //避免windows.h 和 WinSock2.h 中的宏定义重复
+#include <windows.h>
+#include <WinSock2.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//静态链接库 win平台
+//#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    WORD ver = MAKEWORD(2, 2); //socket版本 2.x环境
+    WSADATA data;
+    WSAStartup(ver, &data);
+    //---------------------------
+    //-socket
+    SOCKET _sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); //ipv4, 流数据, tcp
+    if (INVALID_SOCKET == _sock) {
+        printf("socket error \n");
+    } else {
+        printf("socket success \n");
+    }
+    //-connect
+    sockaddr_in _sin = {};
+    _sin.sin_family = AF_INET;
+    _sin.sin_port = htons(4567);
+    _sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+    if (SOCKET_ERROR == connect(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in))) {
+        printf("connect error \n");
+    } else {
+        printf("connect error \n");
+    }
+    // - recv
+    char recvBuf[256] = {};
+    int nlen = recv(_sock, recvBuf, 256, 0);
+    if (nlen > 0) {
+        printf("recv data %s \n", recvBuf);
+    }
+    //-close-
+    closesocket(_sock);
+    //---------------------------
+    WSACleanup();
+    getchar();
+    return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
--- 游戏服务器开发/code/server/server.cpp
+++ 游戏服务器开发/code/server/server.cpp
@@ -1,9 +1,9 @@
 ﻿#include <iostream>
 
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN //避免windows.h 和 WinSock2.h 中的宏定义重复
 #include <windows.h>
 #include <WinSock2.h>
-#include <WS2tcpip.h>
 
 //静态链接库 win平台
 //#pragma comment(lib, "ws2_32.lib")
@@ -16,12 +16,17 @@ int main()
     //---------------------------
     //-socket
     SOCKET _sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); //ipv4, 流数据, tcp
+    if (INVALID_SOCKET == _sock) {
+        printf("socket error \n");
+    } else {
+        printf("socket success \n");
+    }
     //-bind-
     sockaddr_in _sin = {};
     _sin.sin_family = AF_INET;
     _sin.sin_port = htons(4567); //主机数据转换到网络数据 host to net unsigned short
-    _sin.sin_addr.S_un.S_addr = inet_pton(AF_INET, "127.0.0.1", &_sin.sin_addr); //INADDR_ANY; // ip
-    if (SOCKET_ERROR == bind(_sock, (sockaddr*)&_sin, sizeof(_sin))) {
+    _sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1"); //INADDR_ANY; // ip
+    if (SOCKET_ERROR == bind(_sock, (sockaddr*)&_sin, sizeof(sockaddr_in))) {
         printf("bind error \n");
     } else {
         printf("bind success \n");
@@ -34,18 +39,17 @@ int main()
     }
     //-accept-
     sockaddr_in clientAddr = {};
-    int nAddrLen = sizeof(clientAddr);
+    int nAddrLen = sizeof(sockaddr_in);
     SOCKET _cSock = INVALID_SOCKET; //无效socket
 
     char msgBuff[] = "Hello, server";
     while (true) {
         _cSock = accept(_sock, (sockaddr*)&clientAddr, &nAddrLen);
         if (INVALID_SOCKET == _cSock) {
-            printf("error, client invalid socket");
+            printf("error, client invalid socket \n");
         }
 
-        char str[INET_ADDRSTRLEN];
-        printf("new client add: ip = %s /n", inet_ntop(AF_INET, &clientAddr.sin_addr, str, sizeof(str)));
+        printf("new client add: ip = %s \n", inet_ntoa(clientAddr.sin_addr));
         //-send-
         send(_cSock, msgBuff, strlen(msgBuff) + 1, 0);
     }
