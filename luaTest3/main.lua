9875207206f539d43d8686de6be383a2b42add58
sunkang <kang.sun@joywinds.com>
2020-06-09 17:49:39 +0800

    update

--- luaTest3/main.lua
+++ luaTest3/main.lua
@@ -1,7 +1,9 @@
 local json =  require "json"
+local dkjson =  require "dkjson"
 local uihelper =  require "uihelper"
 local result = {sns = "facebook",
                 sns_user_id=999000199,
                 time = 1591693079}
 
-print(uihelper.encodeBase64(json.encode(result))) 
+print(uihelper.encodeBase64(json.encode(result)))
+print(uihelper.encodeBase64(dkjson.encode(result))) 
